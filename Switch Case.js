d284b290004720d429d4253d6fa896300fcac9a1
fatah banan wijaya <fatah.banan@gmail.com>
2018-09-26 22:32:50 +0700

    week2E4-SwitchCase

---
+++ Switch Case.js
@@ -0,0 +1,19 @@
+var tanggal= 27
+var bulan=9
+var tahun= 2018
+
+switch(bulan) {
+
+  case 1:   { console.log(tanggal +' JANUARI '+ tahun); break; }
+  case 2:   { console.log(tanggal +' FEBRUARI '+ tahun); break; }
+  case 3:   { console.log(tanggal +' MARET '+ tahun); break; }
+  case 4:   { console.log(tanggal +' APRIL '+ tahun); break; }
+  case 5:   { console.log(tanggal +' MEI '+ tahun); break; }
+  case 6:   { console.log(tanggal +' JUNI '+ tahun); break; }
+  case 7:   { console.log(tanggal +' JULI '+ tahun); break; }
+  case 8:   { console.log(tanggal +' AGUSTUS '+ tahun); break; }
+  case 9:   { console.log(tanggal +' SEPTEMBER '+ tahun); break; }
+  case 10:   { console.log(tanggal +' OKTOBER '+ tahun); break; }
+  case 11:   { console.log(tanggal +' NOVEMBER '+ tahun); break; }
+  case 12:   { console.log(tanggal +' DESEMBER '+ tahun); break; }
+}
